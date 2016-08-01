b6dc7a7dd47dceea2572311608168429d1c91977
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2016-08-01 21:03:42 +0530

    Base v2 functionality implemented.

--- clogc.js
+++ clogc.js
@@ -1,16 +1,26 @@
 #!/usr/bin/env node
 'use strict'
 
-const rx = require('rx-lite').Observable
+const most = require('most')
 const spawn = require('child_process').spawn
-const color = require('cli-color')
+// const color = require('cli-color')
 const docopt = require('docopt').docopt
 
-function ping (host) {
-  host = host === undefined ? '8.8.8.8' : host
+const buffer = (stream) => {
+  let buffer
+  return stream
+    .map((x) => [buffer, x])
+    .tap(
+      function (bx) {
+        buffer = bx[1]
+      }
+    )
+}
+
+const ping = (host) => {
   switch (process.platform) {
     case 'linux':
-      return rx.fromEvent(spawn('ping', ['-O', host]).stdout, 'data')
+      return most.fromEvent('data', spawn('ping', ['-O', host]).stdout)
         .skip(1)
         .map((data) => data.toString())
         .map((output) => {
@@ -25,8 +35,9 @@ function ping (host) {
               return -3
           }
         })
+        .thru(buffer).filter((bo) => !(bo[0] === -3 && bo[1] === -1)).map((bo) => bo[1])
     case 'win32':
-      return rx.fromEvent(spawn('ping', ['-t', host]).stdout, 'data')
+      return most.fromEvent('data', spawn('ping', ['-t', host]).stdout)
         .skip(1)
         .map((data) => data.toString())
         .map((output) => {
@@ -46,99 +57,54 @@ function ping (host) {
   }
 }
 
-const SYMBOLS = {
-  SPARK: [
-    '▁',
-    '▂',
-    '▃',
-    '▄',
-    '▅',
-    '▆',
-    '▇',
-    '█'
-  ],
-  SLAB: [
-    '▏',
-    '▎',
-    '▍',
-    '▌',
-    '▋',
-    '▊',
-    '▉',
-    '█'
-  ],
-  SHADE: [
-    '░',
-    '▒',
-    '▓',
-    '█'
-  ],
-  BANG: '!',
-  CROSS: '×'
-}
-const PALETTE = {
-  ANSI: {
-    LATENCY: [
-      color.white,
-      color.white,
-      color.yellow
+const clogc = (opts) => {
+  const legend = {
+    latency: [
+      {step: 20, symbol: '●'},
+      {step: 100, symbol: '◉'},
+      {step: Number.POSITIVE_INFINITY, symbol: '○'}
     ],
-    LOSS: color.bgRed,
-    UNREACHABLE: color.bgRedBright,
-    UNKNOWN: color.magenta
-  },
-  XTERM: {
-    LATENCY: [
-      color.xterm(231),
-      color.xterm(231),
-      color.xterm(231),
-      color.xterm(230),
-      color.xterm(230),
-      color.xterm(229),
-      color.xterm(228),
-      color.xterm(220)
-    ],
-    LOSS: color.bgXterm(202),
-    UNREACHABLE: color.bgXterm(160),
-    UNKNOWN: color.xterm(55)
+    connFail: '·',
+    authFail: '/',
+    separator: `—`
   }
-}
-
-function clogc (opts) {
-  opts = opts || {}
-  let host = opts.host
-  let min = opts.max === undefined ? 0 : opts.min
-  let max = opts.max === undefined ? 800 : opts.max
-  let style = opts.style === undefined ? 'SLAB' : opts.style.toUpperCase()
-  return ping(host)
-    .map((log) => { // Normalizing latencies.
-      if (log >= 0) {
-        let latency = Math.log(log - min) / Math.log(max - min)
-        latency = latency < 0 ? 0 : latency >= 1 ? 1 : latency
-        return latency
-      } else {
-        return log
+  return ping(opts.host)
+    .map((ping) => { // Latency normalization.
+      switch (ping) {
+        case -3:
+          return legend.authFail
+        case -2:
+          return legend.connFail
+        case -1:
+          return legend.connFail
+        default:
+          return legend.latency.find((bracket) => ping < bracket.step).symbol
       }
     })
-    .map((log) => { // Visualizing logs.
-      let legend = SYMBOLS[style]
-      let palette = PALETTE[process.platform === 'linux' ? 'XTERM' : 'ANSI']
-      let unknownColor = palette.UNKNOWN
-      let unrechableColor = palette.UNREACHABLE
-      let lossColor = palette.LOSS
-      let latencyColors = palette.LATENCY
-      switch (log) {
-        case -3:
-          return unknownColor(SYMBOLS.BANG)
-        case -2:
-          return unrechableColor(SYMBOLS.CROSS)
-        case -1:
-          return lossColor(SYMBOLS.CROSS)
-        default:
-          let latency = (log * legend.length) | 0
-          latency = latency < 0 ? 0 : latency >= legend.length ? legend.length - 1 : latency
-          return latencyColors[latency](legend[latency])
-      }
+    .thru((ping$) => { // Formatting and timed grouping.
+      let colc = 1
+      return ping$
+        .timestamp()
+        .map((tp) => ({time: new Date(tp.time), value: tp.value}))
+        .thru(buffer)
+        .flatMap((btp) => {
+          let prefix = []
+          if (opts.col > 0) { // If there minute-grouped are columns to draw.
+            if (!btp[0]) {
+              prefix.push(`hh:mm  `)
+              prefix.push(...new Array(btp[1].time.getSeconds() - 1).fill(' '))
+            } else if (btp[0].time.getMinutes() !== btp[1].time.getMinutes()) {
+              prefix.push((colc = ++colc % opts.col) === 0 ? '\n' + `hh:mm  ` : legend.separator)
+            }
+          } else { // Or if it's hour grouped.
+            if (!btp[0]) {
+              prefix.push(`hh:mm  ` + '\n')
+            } else if (btp[0].time.getHours() !== btp[1].time.getHours()) {
+              prefix.push('\n' + `hh:mm  ` + '\n')
+            }
+          }
+          return most.from(prefix.concat(btp[1].value))
+        })
     })
 }
 
@@ -146,18 +112,14 @@ const DOC = `
 Usage: clogc.js [options] [<host>]
 
 Options:
-  --min <min>     Minimum latency normalization limit.  [default: 0]
-  --max <max>     Maximum latency normalization limit.  [default: 800]
-  --style <style> Style of the latency visualization.   [default: slab]
+  --col <col>     Colums to display.  [default: 1]
 `
 
 if (require.main === module) {
   let opts = docopt(DOC)
   opts = {
-    host: opts['<host>'] === null ? undefined : opts['<host>'],
-    min: opts['--min'] === null ? undefined : opts['--min'],
-    max: opts['--max'] === null ? undefined : opts['--max'],
-    style: opts['--style'] === null ? undefined : opts['--style']
+    host: opts['<host>'] || '8.8.8.8',
+    col: Number.parseInt(opts['--col'], 10)
   }
   clogc(opts)
     .forEach((log) => process.stdout.write(log))
