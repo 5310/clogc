a0576184582ce119146cef31d688055ef3047ec1
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2016-08-01 21:31:15 +0530

    Timestamp fixed. Docopts cleaned.

--- clogc.js
+++ clogc.js
@@ -6,6 +6,11 @@ const spawn = require('child_process').spawn
 // const color = require('cli-color')
 const docopt = require('docopt').docopt
 
+const time = (date) => {
+  const pad = (n, width) => `${n}`.length >= width ? `${n}` : new Array(width - `${n}`.length + 1).join('0') + `${n}`
+  return `${pad(date.getHours(), 2)}:${pad(date.getMinutes(), 2)}`
+}
+
 const buffer = (stream) => {
   let buffer
   return stream
@@ -20,38 +25,35 @@ const buffer = (stream) => {
 const ping = (host) => {
   switch (process.platform) {
     case 'linux':
-      return most.fromEvent('data', spawn('ping', ['-O', host]).stdout)
+      return most.fromEvent('data', spawn('ping', ['-D', '-O', host]).stdout)
         .skip(1)
         .map((data) => data.toString())
-        .map((output) => {
-          switch (true) {
-            case (output.startsWith('64 bytes')):
-              return Number.parseFloat(output.match(/time=(\d+(\.\d+)?) ms/)[1])
-            case (output.startsWith('no answer')):
-              return -1
-            case (output.search('Destination Host Unreachable') > -1):
-              return -2
-            default:
-              return -3
+        .map((string) => { // Parse timestamps.
+          const tokens = string.split(' ')
+          return {
+            time: new Date(Number.parseFloat(tokens[0].slice(1, -1)) * 1000),
+            value: tokens.slice(1).join(' ')
           }
         })
-        .thru(buffer).filter((bo) => !(bo[0] === -3 && bo[1] === -1)).map((bo) => bo[1])
-    case 'win32':
-      return most.fromEvent('data', spawn('ping', ['-t', host]).stdout)
-        .skip(1)
-        .map((data) => data.toString())
-        .map((output) => {
+        .map((output) => { // Parse latency and failure status.
           switch (true) {
-            case (output.startsWith('Reply from')):
-              return Number.parseFloat(output.match(/time=(\d+)ms/)[1])
-            case (output.startsWith('Request timed out')):
-              return -1
-            case (output.startsWith('Destination host unreachable')):
-              return -2
+            case (output.value.startsWith('64 bytes')):
+              output.value = Number.parseFloat(output.value.match(/time=(\d+(\.\d+)?) ms/)[1])
+              break
+            case (output.value.startsWith('no answer')):
+              output.value = -1
+              break
+            case (output.value.search('Destination Host Unreachable') > -1):
+              output.value = -2
+              break
             default:
-              return -3
+              output.value = -3
           }
+          return output
         })
+        .thru(buffer).filter((bo) => { // Filter out redundant time outs for auth failures.
+          return bo[0] ? !(bo[0].value === -3 && bo[1].value === -1) : true
+        }).map((bo) => bo[1])
     default:
       throw new Error('OS not supported')
   }
@@ -70,40 +72,42 @@ const clogc = (opts) => {
   }
   return ping(opts.host)
     .map((ping) => { // Latency normalization.
-      switch (ping) {
+      switch (ping.value) {
         case -3:
-          return legend.authFail
+          ping.value = legend.authFail
+          break
         case -2:
-          return legend.connFail
+          ping.value = legend.connFail
+          break
         case -1:
-          return legend.connFail
+          ping.value = legend.connFail
+          break
         default:
-          return legend.latency.find((bracket) => ping < bracket.step).symbol
+          ping.value = legend.latency.find((bracket) => ping.value < bracket.step).symbol
       }
+      return ping
     })
     .thru((ping$) => { // Formatting and timed grouping.
-      let colc = 1
+      let colc = 0
       return ping$
-        .timestamp()
-        .map((tp) => ({time: new Date(tp.time), value: tp.value}))
         .thru(buffer)
-        .flatMap((btp) => {
+        .flatMap((bp) => {
           let prefix = []
           if (opts.col > 0) { // If there minute-grouped are columns to draw.
-            if (!btp[0]) {
-              prefix.push(`hh:mm  `)
-              prefix.push(...new Array(btp[1].time.getSeconds() - 1).fill(' '))
-            } else if (btp[0].time.getMinutes() !== btp[1].time.getMinutes()) {
-              prefix.push((colc = ++colc % opts.col) === 0 ? '\n' + `hh:mm  ` : legend.separator)
+            if (!bp[0]) {
+              prefix.push(time(bp[1].time) + '  ')
+              prefix.push(...new Array(bp[1].time.getSeconds()).fill(' '))
+            } else if (bp[0].time.getMinutes() !== bp[1].time.getMinutes()) {
+              prefix.push((colc = ++colc % opts.col) === 0 ? '\n' + time(bp[1].time) + '  ' : legend.separator)
             }
           } else { // Or if it's hour grouped.
-            if (!btp[0]) {
-              prefix.push(`hh:mm  ` + '\n')
-            } else if (btp[0].time.getHours() !== btp[1].time.getHours()) {
-              prefix.push('\n' + `hh:mm  ` + '\n')
+            if (!bp[0]) {
+              prefix.push(time(bp[1].time) + '  ' + '\n')
+            } else if (bp[0].time.getHours() !== bp[1].time.getHours()) {
+              prefix.push('\n' + time(bp[1].time) + '  ' + '\n')
             }
           }
-          return most.from(prefix.concat(btp[1].value))
+          return most.from(prefix.concat(bp[1].value))
         })
     })
 }
@@ -112,7 +116,7 @@ const DOC = `
 Usage: clogc.js [options] [<host>]
 
 Options:
-  --col <col>     Colums to display.  [default: 1]
+  -c --col <col>     Colums to display.  [default: 1]
 `
 
 if (require.main === module) {
